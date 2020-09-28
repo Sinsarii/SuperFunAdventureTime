bccee75df0be56870060840258324146bb83b88b
Nikolas Mikolaski <mikolaskin@gmail.com>
2020-09-28 16:47:27 -0400

    Added classes for the basic functions of creating the player, monsters, items, and locations. created the parent classes LivingCreatures and Item to avoid duplicate code with inheritance. //TODO// Add the Quest and Item lists to quest class and player class.

--- SuperFunAdventureTime.cs
+++ SuperFunAdventureTime.cs
@@ -7,19 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Engine;
 
 namespace SuperFunAdventureTime
 {
     public partial class SuperFunAdventureTime : Form
     {
+        private Player _player;
         public SuperFunAdventureTime()
         {
             InitializeComponent();
-        }
 
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
+            _player = new Player(10,10,20,0,1);
+
+            Location location = new Location(1, "Home", "This is your house.");
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
-}
+ }
