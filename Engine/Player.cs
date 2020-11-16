042d604e9ecf1f0c4d374579710890cc19fbc87d
Nikolas Mikolaski <mikolaskin@gmail.com>
2020-11-16 16:09:42 -0500

    --Refactoring-- -Refactored code by moving functions from the superFun class that were more player specific to the player class itself. This will make it easier to edit anything down the line and keep a seperation of function.

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -24,5 +24,133 @@ namespace Engine
             Quests = new List<PlayerQuest>();
 
         }
+        public bool HasRequiredItemToEnterThisLocation(Location location)
+        {
+            if(location.ItemRequiredToEnter == null)
+            {
+                //there is no required item for this location, so return true
+                return true;
+            }
+
+            //see if player has the required item in their inventory
+            foreach(InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == location.ItemRequiredToEnter.ID)
+                {
+                    //we found the required item, so return "true"
+                    return true;
+                }
+            }
+
+            //We didnt find the required item in their inventory, so return "false"
+            return false;
+        }
+
+        public bool HasThisQuest(Quest quest)
+        {
+            foreach(PlayerQuest playerquest in Quests)
+            {
+                if(playerquest.Details.ID == quest.ID)
+                {
+                    return playerquest.IsCompleted;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CompletedThisQuest(Quest quest)
+        {
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                if(playerQuest.Details.ID == quest.ID)
+                {
+                    return playerQuest.IsCompleted;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasAllQuestCompletionItems(Quest quest)
+        {
+            // see if the player has all the items needed to complete the quest here
+            foreach(QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                bool foundItemInPlayersInventory = false;
+
+                //check each item in the player's inventory to see if they have the item and enough of it
+                foreach(InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID) //the player has the item in their inventory
+                    {
+                        foundItemInPlayersInventory = true;
+
+                        if(ii.Quantity < qci.Quantity) //the player does not have enough of the item to complete the quest
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                // the player does not have any of this quest completion item in their inventory
+                if(!foundItemInPlayersInventory)
+                {
+                    return false;
+                }
+            }
+
+            //if we got here, then the player must have all the required items, and enough of them, to complete the quest.
+            return true;
+        }
+
+        public void RemoveQuestCompletionItems(Quest quest)
+        {
+            foreach(QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                foreach(InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID)
+                    {
+                        //subtract the quantity from the player's inventory that was needed to complete the quest
+                        ii.Quantity -= qci.Quantity;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void AddItemToInventory(Item itemToAdd)
+        {
+            foreach(InventoryItem ii in Inventory)
+            {
+                if(ii.Details.ID == itemToAdd.ID)
+                {
+                    //if they already have them same item in their inventory, increase the quantity by one
+                    ii.Quantity++;
+
+                    return; //we added the item, and are done, so get out of this function
+                }
+            }
+            //they didn't have the item, so add it to their inventory, with a quantity of 1
+            Inventory.Add(new InventoryItem(itemToAdd, 1));
+        }
+
+        public void MarkQuestCompleted(Quest quest)
+        {
+            //Find the quest in the player's quest list
+            foreach(PlayerQuest pq in Quests)
+            {
+                if(pq.Details.ID == quest.ID)
+                {
+                    //Mark it as completed
+                    pq.IsCompleted = true;
+
+                    return; //We found the quest, and marked it complete, so get out of this function
+                }
+            }
+        }
     }
 }
+
+
--- SuperFunAdventureTime.cs
+++ SuperFunAdventureTime.cs
@@ -27,6 +27,7 @@ namespace SuperFunAdventureTime
             lblGold.Text = _player.Gold.ToString();
             lblExperience.Text = _player.ExperiencePoints.ToString();
             lblLevel.Text = _player.Level.ToString();
+
         }
 
         private void btnNorth_Click(object sender, EventArgs e)
@@ -48,41 +49,15 @@ namespace SuperFunAdventureTime
         {
 
         }
+       
 
-        private void btnUseWeapon_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnUsePotion_Click(object sender, EventArgs e)
-        {
-
-        }
-        
         private void MoveTo(Location newLocation)
         {
             //Does the location have an required items before entering
-            if(newLocation.ItemRequiredToEnter != null)
+            if(!_player.HasRequiredItemToEnterThisLocation(newLocation))
             {
-                //see if the player has the required item in their inventory
-                bool playerHasRequiredItem = false;
-
-                foreach(InventoryItem ii in _player.Inventory)
-                {
-                    if(ii.Details.ID == newLocation.ItemRequiredToEnter.ID)
-                    {
-                        //We found the required Item
-                        playerHasRequiredItem = true;
-                        break; //exit our of foreach loop
-                    }
-                }
-
-                if(!playerHasRequiredItem)
-                {
-                    //The required item was not found in the players inventory, display message and halt movement
-                    rtbMessages.Text += "You must have a" + newLocation.ItemRequiredToEnter.Name + " to enter this location." + Environment.NewLine;
-                    return;
-                }
+                rtbMessages.Text += "You must have a " + newLocation.ItemRequiredToEnter.Name + " to enter this location." + Environment.NewLine;
+                return;
             }
 
             //Update the player's current location
@@ -108,66 +83,16 @@ namespace SuperFunAdventureTime
             if(newLocation.QuestAvailableHere != null)
             {
                 //see if the player already has the quest, and if they've completed it
-                bool playerAlreadyHasQuest = false;
-                bool playerAlreadyCompletedQuest = false;
-
-                foreach(PlayerQuest playerQuest in _player.Quests)
-                {
-                    if(playerQuest.Details.ID == newLocation.QuestAvailableHere.ID)
-                    {
-                        playerAlreadyHasQuest = true;
-
-                        if(playerQuest.IsCompleted)
-                        {
-                            playerAlreadyCompletedQuest = true;
-                        }
-
-                    }
-                }
-                //see if the player already has the quest
+                bool playerAlreadyHasQuest = _player.HasThisQuest(newLocation.QuestAvailableHere);
+                bool playerAlreadyCompletedQuest = _player.CompletedThisQuest(newLocation.QuestAvailableHere); 
+                                
                 if (playerAlreadyHasQuest)
                 {
                     //if the player has not completed the quest yet
                     if(!playerAlreadyCompletedQuest)
                     {
-                        //see if the player has all the items needed to complete the quest
-                        bool playerHasAllItemsToCompleteQuest = true;
-
-                        foreach(QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompletionItems)
-                        {
-                            bool foundItemInPlayersInventory = false;
-
-                            //check each item in the players inventory too see: if the have it and if they have enough of it
-                            foreach(InventoryItem ii in _player.Inventory)
-                            {
-                                //the player has this item in their inventory
-                                if(ii.Details.ID == qci.Details.ID)
-                                {
-                                    foundItemInPlayersInventory = true;
-
-                                    if(ii.Quantity < qci.Quantity)
-                                    {
-                                        //the player does not have enough of this item to complete the quest
-                                        playerHasAllItemsToCompleteQuest = false;
-
-                                        //there is no reason to continue checking for the other quest completion items
-                                        break;
-                                    }
-                                    //we found the item, so don't check the rest of the players inventory
-                                    break;
-                                }
-                            }
-                            //if we didn't find the required item, set our variable to stop looking for other items
-                            if(!foundItemInPlayersInventory)
-                            {
-                                //the player does not have this item in their inventory
-                                playerHasAllItemsToCompleteQuest = false;
-
-                                //there is no reason to continue checking for the other quest completion items
-                                break;
-                            }
-                        }
-                        //the player has all the items required to complete the quest
+                        bool playerHasAllItemsToCompleteQuest = _player.HasAllQuestCompletionItems(newLocation.QuestAvailableHere);
+                        
                         if(playerHasAllItemsToCompleteQuest)
                         {
                             //display message
@@ -175,18 +100,8 @@ namespace SuperFunAdventureTime
                             rtbMessages.Text += "You complete the  '" + newLocation.QuestAvailableHere.Name + "'quest." + Environment.NewLine;
 
                             //remove quest items from inventory
-                            foreach(QuestCompletionItem qci in newLocation.QuestAvailableHere.QuestCompletionItems)
-                            {
-                                foreach (InventoryItem ii in _player.Inventory)
-                                {
-                                    if (ii.Details.ID == qci.Details.ID)
-                                    {
-                                        //subtract the quantity from the player's inventory that was needed to complete the quest
-                                        ii.Quantity -= qci.Quantity;
-                                        break;
-                                    }
-                                }
-                            }
+                            _player.RemoveQuestCompletionItems(newLocation.QuestAvailableHere);
+                            
 
                             //give quest rewards!
                             rtbMessages.Text += "You recieve: " + Environment.NewLine;
@@ -199,39 +114,13 @@ namespace SuperFunAdventureTime
                             _player.Gold += newLocation.QuestAvailableHere.RewardGold;
 
                             //add the reward item to the player's inventory
-                            bool addItemToPlayerInventory = false;
+                            _player.AddItemToInventory(newLocation.QuestAvailableHere.RewardItem);
 
-                            foreach(InventoryItem ii in _player.Inventory)
-                            {
-                                if(ii.Details.ID == newLocation.QuestAvailableHere.RewardItem.ID)
-                                {
-                                    //the have the item in their inventory, so increase the quantity by 1
-                                    ii.Quantity++;
-
-                                    addItemToPlayerInventory = true;
-
-                                    break;
-                                }
-                            }
-
-                            //the didn't have the item, so add it to their inventory with a quantity of 1
-                            if(!addItemToPlayerInventory)
-                            {
-                                _player.Inventory.Add(new InventoryItem(newLocation.QuestAvailableHere.RewardItem, 1));
-                            }
 
                             //mark the quest as completed
                             //find the quest in the players quest list
-                            foreach(PlayerQuest pq  in _player.Quests)
-                            {
-                                if (pq.Details.ID == newLocation.QuestAvailableHere.ID)
-                                {
-                                    //mark it as completed
-                                    pq.IsCompleted = true;
-
-                                    break;
-                                }
-                            }
+                            _player.MarkQuestCompleted(newLocation.QuestAvailableHere);
+                            
                         }
                     }
                 }
